c21faea32aa628084b399f9d569bf20ab6393093
pinto <pintonsebastian1@gmail.com>
2026-06-05 13:52:57 +0530

    postman connection

--- day4/backend/routes/userRoutes.js
+++ day4/backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 const express=require('express');
 const router=express.Router();
 const userModel=require('../models/userModel');
-
+//API for fetching or reading the data from DB
 router.get('/',async (req, res) =>{
 const users=await userModel.find();
 res.json(users);
 });
+//API for posting the new document in the collection
+router.post('/newstudent',async(req,res)=>{
+    var item = req.body;//extract the data from the request object
+    const datasave=new userModel(item);
+    const savedata=await datasave.save();//save data to collection
+    res.send('post successful');
+
+})
 
 module.exports = router;
